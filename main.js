28818c3fe8ecd4683e3a802e08de5ccca2306d9f
Mohamed-Eddib <dahmouneeddib@gmail.com>
2025-11-17 14:17:50 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+document.getElementById("Btn").addEventListener("click", function (e) {
+    e.preventDefault();
+    convert(); 
+});
+
+let Btn = document.getElementById("Btn");
+let toFahrenheit = document.getElementById("toCelsius");
+let toCelsius = document.getElementById("toFahrenheit");
+let first_value ;
+let converted_value ;
+let label = document.getElementById("converted_value");
+Btn.onclick = function(){
+    if(toCelsius.checked){
+        first_value = document.getElementById("first_value").value;
+        converted_value = (first_value - 32 ) * (5/9) ;
+        label.textContent = Math.round(converted_value) + "°" ;
+
+    }
+    else if(toFahrenheit.checked){
+        first_value = document.getElementById("first_value").value;
+        converted_value = (first_value * (9/5) ) + 32 ;
+        label.textContent = Math.round(converted_value) + "°" ;
+
+    }
+    else{
+        label.textContent = "Please choose a valid conversion method";
+    }
+}
