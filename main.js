e7f0a441a7f264806d4390981f3070703325d476
Mohamed-Eddib <dahmouneeddib@gmail.com>
2025-11-18 16:22:29 +0100

    Update main.js

--- main.js
+++ main.js
@@ -10,19 +10,25 @@ let first_value ;
 let converted_value ;
 let label = document.getElementById("converted_value");
 Btn.onclick = function(){
-    if(toCelsius.checked){
-        first_value = document.getElementById("first_value").value;
-        converted_value = (first_value - 32 ) * (5/9) ;
-        label.textContent = Math.round(converted_value) + "°" ;
-
-    }
-    else if(toFahrenheit.checked){
-        first_value = document.getElementById("first_value").value;
-        converted_value = (first_value * (9/5) ) + 32 ;
-        label.textContent = Math.round(converted_value) + "°" ;
-
+    first_value = document.getElementById("first_value").value;
+    if(first_value === NaN){
+        label.textContent = Please enter a valid number;
     }
     else{
-        label.textContent = "Please choose a valid conversion method";
-    }
+     if(toCelsius.checked){
+         converted_value = (first_value - 32 ) * (5/9) ;
+         label.textContent = Math.round(converted_value) + "°" ;
+
+     }
+     else if(toFahrenheit.checked){
+         first_value = document.getElementById("first_value").value;
+         converted_value = (first_value * (9/5) ) + 32 ;
+         label.textContent = Math.round(converted_value) + "°" ;
+
+     }
+     else{
+         label.textContent = "Please choose a valid conversion method";
+     }
+ }
 }
+
